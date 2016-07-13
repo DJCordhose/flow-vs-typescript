04443a127fd089f874f2f9a731ddb7ff56846658
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-07-13 11:22:20 +0200

    react type declarations now managed with npm

--- react-typescript/typings/index.d.ts
+++ react-typescript/typings/index.d.ts
@@ -2,6 +2,4 @@
 /// <reference path="globals/expect/index.d.ts" />
 /// <reference path="globals/immutable/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
-/// <reference path="globals/react-dom/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
 /// <reference path="globals/whatwg-fetch/index.d.ts" />
