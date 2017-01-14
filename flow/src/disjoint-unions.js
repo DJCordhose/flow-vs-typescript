c00da2c6d2363415ee61f287db488b639bee8ed0
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-01-14 21:44:28 +0100

    changed `Makes it easier for people coming from Java/C++/C#` to `Similar features can be found in Java/C++/C#` to reduce chance of getting a negative connotation (which was never intended). Thanks to Jim Geppert

--- flow/src/disjoint-unions.js
+++ flow/src/disjoint-unions.js
@@ -1,1 +1,28 @@
 // https://flowtype.org/docs/disjoint-unions.html
+
+/* @flow */
+
+type Resp = Res | Err; // a disjoint union type with two cases
+type Res = { status: 'done', payload: Object };
+type Err = { status: 'error', message: string };
+
+function callback(response: Resp) {
+    // given by code compeletion
+    switch (response.status) {
+        case 'done':
+            // this is the special thing: we now know, this is a Response
+            const result: Res = response;
+            console.log(result.payload);
+            break;
+        case 'error':
+            const error: Err = response;
+            console.log(error.message);
+            break;
+    }
+}
+
+// let aha: Resp;
+// if (aha.status === 'done') {
+//     const oho: Res = Resp;
+// }
+
