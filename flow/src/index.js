a3d196bbc6a7d14b3d33bebec0a624d09abd95ce
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-02-07 20:22:46 +0100

    walktrough

--- flow/src/index.js
+++ flow/src/index.js
@@ -4,9 +4,10 @@
 // import './thirdparty-example';
 // import './basics';
 // import './readonly';
-import './sandbox';
+// import './sandbox';
 // import './structural-nominal';
 // import './any';
 // import './jsConsumer';
 // import './controlFlow';
 // import './generics-invariant';
+import './walkthrough';
