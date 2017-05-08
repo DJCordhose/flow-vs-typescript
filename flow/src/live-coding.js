78aae2d0a26e4de978a0952a93f461985eb0a7ac
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2017-05-08 15:06:38 +0200

    Details for JAX

--- flow/src/live-coding.js
+++ flow/src/live-coding.js
@@ -1,3 +1,21 @@
-let a = 10;
+// @flow
 
+let a: number = 10;
 a = true;
+
+function gruss(name: string) {
+    return "Hallo, " + name; 
+}
+
+const ergebnis: string = gruss(10);
+
+class Sayer {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    gruss(): string {
+        return "Hallo, " + this.name;
+    }
+}
